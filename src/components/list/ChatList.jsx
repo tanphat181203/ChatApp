dd37265b7026c4b94365ad6888751ede8f2b60e3
Phat <tanphat181203@gmail.com>
2024-07-30 18:38:21 +0700

    change scrollbar

--- src/components/list/ChatList.jsx
+++ src/components/list/ChatList.jsx
@@ -4,7 +4,7 @@ export default function ChatList() {
   const [addButtonMode, setAddButtonMode] = useState(false);
 
   return (
-    <div className="flex-1 overflow-scroll">
+    <div className="flex-1 overflow-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-dark-blue-rgba scrollbar-track-transparent">
       <div className="flex items-center gap-5 p-5">
         <div className="flex-1 flex items-center gap-5 p-2 bg-dark-blue-rgba-1 rounded-xl">
           <img className="w-5 h-5" src="/search.png" alt="" />
@@ -22,7 +22,11 @@ export default function ChatList() {
         />
       </div>
       <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-gray-500">
-        <img className="w-[50px] h-[50px] rounded-full object-cover" src="/avatar.png" alt="" />
+        <img
+          className="w-[50px] h-[50px] rounded-full object-cover"
+          src="/avatar.png"
+          alt=""
+        />
         <div className="flex flex-col gap-[10px]">
           <span className="font-medium">Lorem Ipsum</span>
           <p className="text-sm font-light">Hello</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'dark-blue-rgba': 'rgba(17, 25, 40, 0.75)',
-        'dark-blue-rgba-1': 'rgba(17, 25, 40, 0.50)',
+        "dark-blue-rgba": "rgba(17, 25, 40, 0.75)",
+        "dark-blue-rgba-1": "rgba(17, 25, 40, 0.50)",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
