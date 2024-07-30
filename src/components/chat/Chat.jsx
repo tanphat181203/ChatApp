35795f77238d3a828851cd1d841edf2d83d422d0
Phat <tanphat181203@gmail.com>
2024-07-30 20:16:27 +0700

    chat header

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,3 +1,28 @@
 export default function Chat() {
-  return <div className="flex-1">Chat</div>
+  return (
+    <div className="flex-2 border-l border-r border-gray-500 h-full">
+      <div className="p-5 flex items-center justify-between border-b border-gray-500">
+        <div className="flex items-center gap-5">
+          <img
+            className="w-[60px] h-[60px] rounded-full object-cover"
+            src="/avatar.png"
+            alt=""
+          />
+          <div className="flex flex-col gap-1">
+            <span className="text-lg font-bold">Felix Kyun</span>
+            <p className="text-sm font-light text-gray-400">
+              Lorem ipsum dolor sit amet consectetur!
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-5">
+          <img className="w-5 h-5" src="/phone.png" alt="" />
+          <img className="w-5 h-5" src="/video.png" alt="" />
+          <img className="w-5 h-5" src="/info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         "dark-blue-rgba": "rgba(17, 25, 40, 0.75)",
         "dark-blue-rgba-1": "rgba(17, 25, 40, 0.50)",
       },
+      flex: {
+        2: "2 2 0%",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar")],
